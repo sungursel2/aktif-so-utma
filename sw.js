f9a328f0168e4b44b2bad4408d40616cc40a7484
sungursel2 <sungursel2@gmail.com>
2026-04-07 00:37:31 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE = 'aktif-sogutma-v1';
-const FILES = ['./index.html','./manifest.json','./products.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE='aktif-sogutma-v2';
+const FILES=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
